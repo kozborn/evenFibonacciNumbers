55c442972351f4e4da787c69919351da33e53872
Piotr Kozubek <piotrkozubek@gmail.com>
2015-03-31 12:14:05 +0200

    Finished task

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int a = 0,b = 1;
+    long int sum = 0;
+    int top = 40000;
+    cout << "Even fibonacci number sum!" << endl;
+    while(sum < top){
+        b += a;
+        a = b - a;
+        if((b % 2) == 0){
+            sum += b;
+        }
+    }
+    cout << "Sum is: " << sum << endl;
+    cout << endl;
     return 0;
 }
 
